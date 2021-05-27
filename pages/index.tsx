2f3e0ad9a463aadc2e8532c4d6f763f791423572
igordreher <igord.sparrenberger@gmail.com>
2021-05-26 22:25:37 -0300

    Add github oath

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
 import Head from 'next/head';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 export default function Home() {
+  const [session] = useSession();
+
   return (
     <div>
       <Head>
         <title>Github Tags</title>
       </Head>
+      {!session && <>
+        <button onClick={() => signIn()}> Login with GitHub</button>
+      </>
+      }{session && <>
+        <button onClick={() => signOut()}> Sign Out</button>
+      </>
+      }
     </div>
   );
 }
