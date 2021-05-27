2e94287a9d2396e25f9a781201fa65671e33e587
igordreher <igord.sparrenberger@gmail.com>
2021-05-27 15:01:41 -0300

    Add prisma connection to next-auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,21 @@
+import { NextApiHandler } from 'next';
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import Adapters from "next-auth/adapters";
+import prisma from '../../../lib/prisma';
 
-export default NextAuth({
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+
+const options = {
     providers: [
         Providers.GitHub({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
-            scope: '(no scope)'
+            scope: '(read:user)',
         })
-    ]
-});
+    ],
+
+    adapter: Adapters.Prisma.Adapter({ prisma })
+};
+
+export default authHandler;
