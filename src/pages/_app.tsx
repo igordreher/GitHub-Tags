913aebfa3420309dce5e559d31522286696f9a42
igordreher <igord.sparrenberger@gmail.com>
2021-05-28 16:29:36 -0300

    Remove ';' from _app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import '../styles/globals.scss';
 function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </Provider>
   );
 }
