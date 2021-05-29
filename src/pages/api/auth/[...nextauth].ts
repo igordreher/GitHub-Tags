523e10358899bf6ec68525fa481aa483511208eb
igordreher <igord.sparrenberger@gmail.com>
2021-05-29 15:12:03 -0300

    Add userId to next-auth session

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,12 @@
 import { NextApiHandler } from 'next';
-import NextAuth from 'next-auth';
+import NextAuth, { NextAuthOptions } from 'next-auth';
 import Providers from 'next-auth/providers';
 import Adapters from "next-auth/adapters";
 import prisma from '../../../lib/prisma';
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 
-const options = {
+const options: NextAuthOptions = {
     providers: [
         Providers.GitHub({
             clientId: process.env.GITHUB_ID,
@@ -14,7 +14,12 @@ const options = {
             scope: '(read:user)',
         })
     ],
-
+    callbacks: {
+        session: async (session, user) => {
+            session.id = user.id;
+            return Promise.resolve(session);
+        }
+    },
     adapter: Adapters.Prisma.Adapter({ prisma })
 };
 
