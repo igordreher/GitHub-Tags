5f37e2a460360f7aa0b74267eea4bfc80ddd2277
igordreher <igord.sparrenberger@gmail.com>
2021-05-30 18:53:32 -0300

    Header: Fix text-overflow in menu & add strong to user name

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,7 +15,9 @@ export default function Header() {
             <img src={session.user.image} alt="User Avatar" className={styles.userAvatar} sizes="20" />
           </summary>
           <menu className={styles.detailsMenu}>
-            <div className={styles.menuItem}>Signed in as {session.user.name}</div>
+            <div className={styles.menuItem}>
+              Signed in as <strong>{session.user.name}</strong>
+            </div>
             <div className={styles.menuDivider}></div>
             <button className={styles.menuItem} onClick={() => signOut()} >Sign out</button>
           </menu>
