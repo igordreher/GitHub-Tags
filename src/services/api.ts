38ec4622bd6ca3fed163c889625227544a701109
igordreher <igord.sparrenberger@gmail.com>
2021-05-31 12:50:02 -0300

    Update api baseURL to relative path

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,3 @@
 import axios from 'axios';
 
-const baseURL = process.env.BASE_API_URL || 'http://localhost:3000/api/';
-
-export default axios.create({ baseURL });
+export default axios.create({ baseURL: '/api/' });
